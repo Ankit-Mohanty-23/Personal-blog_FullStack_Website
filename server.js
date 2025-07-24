0752325f8992a1e725dedbdc17b581a335765ae6
shadowAliveHere <106421313+shadowAliveHere@users.noreply.github.com>
2025-07-24 20:41:51 +0530

    Fix: Add CORS for Vercel frontend

--- server.js
+++ server.js
@@ -9,20 +9,19 @@ import connectdb from "./database/db.js";
 
 const app = express();
 
-const whitelist = [process.env.FRONTEND_URL];
+const allowedOrigins = [process.env.FRONTEND_URL];
 
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-    optionsSuccessStatus: 200
-};
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true
+}));
 
-app.use(cors(corsOptions));
 app.use(express.json());
 
 app.use('/api/posts', postRoutes);
